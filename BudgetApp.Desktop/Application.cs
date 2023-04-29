a046bb98e2e4f6d894328b46065451222dee2f8d
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-29 12:13:40 +0200

    Refactor entry point

---
+++ BudgetApp.Desktop/Application.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetApp.Desktop
+{
+    public class Application
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
