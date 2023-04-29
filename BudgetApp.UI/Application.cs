1ce58247cc96470a773bd03eb45b00c1764ef382
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-29 14:55:35 +0200

    Change entry point

---
+++ BudgetApp.UI/Application.cs
@@ -0,0 +1,12 @@
+namespace BudgetApp.UI
+{
+    public class Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
