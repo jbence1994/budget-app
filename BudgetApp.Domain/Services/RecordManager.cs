dd24f3937366deb065af25314dfc5e90ff62e44c
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-29 17:54:19 +0200

    Add fake methods

--- BudgetApp.Domain/Services/RecordManager.cs
+++ BudgetApp.Domain/Services/RecordManager.cs
@@ -4,14 +4,36 @@ namespace BudgetApp.Domain.Services
 {
     public class RecordManager
     {
-        public IncomeRecord CreateIncomeRecord()
+        public IncomeRecord CreateIncomeRecord(
+            double amount,
+            string source,
+            IncomeCategory incomeCategory,
+            string comment
+        )
         {
-            return new IncomeRecord();
+            return new IncomeRecord
+            {
+                Amount = amount,
+                Source = source,
+                Category = incomeCategory,
+                Comment = comment,
+                CreatedDateTime = DateTime.Now
+            };
         }
 
-        public ExpenseRecord CreateExpenseRecord()
+        public ExpenseRecord CreateExpenseRecord(
+            double amount,
+            ExpenseCategory expenseCategory,
+            string comment
+        )
         {
-            return new ExpenseRecord();
+            return new ExpenseRecord
+            {
+                Amount = amount,
+                Category = expenseCategory,
+                Comment = comment,
+                CreatedDateTime = DateTime.Now
+            };
         }
     }
 }
