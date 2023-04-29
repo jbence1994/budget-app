2381d81c511cd51b1ce01ca354be64d09b3cd0fd
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-29 15:34:00 +0200

    Add extra props

--- BudgetApp.Core/Models/Record.cs
+++ BudgetApp.Core/Models/Record.cs
@@ -2,7 +2,9 @@ namespace BudgetApp.Core.Models
 {
     public class Record
     {
+        public double Amount { get; set; }
         public Currency Currency { get; set; }
         public TransactionType TransactionType { get; set; }
+        public DateTime CreatedDateTime { get; set; }
     }
 }
