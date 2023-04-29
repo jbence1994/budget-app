46297b1d68f2cb3dc8a5a475917ac8ffdebc45b7
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-29 17:46:15 +0200

    Add main functions to RecordManager

--- BudgetApp.Domain/Services/RecordManager.cs
+++ BudgetApp.Domain/Services/RecordManager.cs
@@ -1,6 +1,17 @@
+using BudgetApp.Core.Models;
+
 namespace BudgetApp.Domain.Services
 {
     public class RecordManager
     {
+        public IncomeRecord CreateIncomeRecord()
+        {
+            return new IncomeRecord();
+        }
+
+        public ExpenseRecord CreateExpenseRecord()
+        {
+            return new ExpenseRecord();
+        }
     }
 }
