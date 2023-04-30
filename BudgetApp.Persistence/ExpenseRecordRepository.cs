77bd3cf3017357671215fe07ac572c1b3bc67749
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-30 14:21:42 +0200

    Add repository implementations for interfaces

---
+++ BudgetApp.Persistence/ExpenseRecordRepository.cs
@@ -0,0 +1,13 @@
+﻿using BudgetApp.Core.Models;
+using BudgetApp.Core.Repositories;
+
+namespace BudgetApp.Persistence
+{
+    public class ExpenseRecordRepository : IExpenseRecordRepository
+    {
+        public ExpenseRecord AddExpenseRecord(ExpenseRecord expenseRecord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BudgetApp.Persistence/Repositories/IncomeRecordRepository.cs
@@ -0,0 +1,13 @@
+﻿using BudgetApp.Core.Models;
+using BudgetApp.Core.Repositories;
+
+namespace BudgetApp.Persistence.Repositories
+{
+    public class IncomeRecordRepository : IIncomeRecordRepository
+    {
+        public IncomeRecord AddIncomeRecord(IncomeRecord incomeRecord)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
