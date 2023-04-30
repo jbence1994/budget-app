a7c85de46fe5f9560c749aaae1033e7ad0b112d0
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-30 14:15:52 +0200

    Add repository interfaces

---
+++ BudgetApp.Core/Repositories/IIncomeRecordRepository.cs
@@ -0,0 +1,9 @@
+﻿using BudgetApp.Core.Models;
+
+namespace BudgetApp.Core.Repositories
+{
+    public interface IIncomeRecordRepository
+    {
+        IncomeRecord AddIncomeRecord(IncomeRecord incomeRecord);
+    }
+}
