4b0c1c50bf037aa94957b428899c234bf4abfeb8
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-30 11:54:34 +0200

    Starting to add base buttons for creating income and expense records

--- BudgetApp.UI/Windows/MainWindow.Designer.cs
+++ BudgetApp.UI/Windows/MainWindow.Designer.cs
@@ -28,7 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonCreateIncomeRecord = new System.Windows.Forms.Button();
+            this.buttonCreateExpenseRecord = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonCreateIncomeRecord
+            // 
+            this.buttonCreateIncomeRecord.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCreateIncomeRecord.Location = new System.Drawing.Point(12, 720);
+            this.buttonCreateIncomeRecord.Name = "buttonCreateIncomeRecord";
+            this.buttonCreateIncomeRecord.Size = new System.Drawing.Size(181, 36);
+            this.buttonCreateIncomeRecord.TabIndex = 0;
+            this.buttonCreateIncomeRecord.Text = "Bevétel hozzáadása";
+            this.buttonCreateIncomeRecord.UseVisualStyleBackColor = true;
+            this.buttonCreateIncomeRecord.Click += new System.EventHandler(this.ButtonCreateIncomeRecord_Click);
+            // 
+            // buttonCreateExpenseRecord
+            // 
+            this.buttonCreateExpenseRecord.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonCreateExpenseRecord.Location = new System.Drawing.Point(831, 720);
+            this.buttonCreateExpenseRecord.Name = "buttonCreateExpenseRecord";
+            this.buttonCreateExpenseRecord.Size = new System.Drawing.Size(181, 36);
+            this.buttonCreateExpenseRecord.TabIndex = 1;
+            this.buttonCreateExpenseRecord.Text = "Kiadás hozzáadása";
+            this.buttonCreateExpenseRecord.UseVisualStyleBackColor = true;
+            this.buttonCreateExpenseRecord.Click += new System.EventHandler(this.ButtonCreateExpenseRecord_Click);
             // 
             // MainWindow
             // 
@@ -36,6 +60,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.buttonCreateExpenseRecord);
+            this.Controls.Add(this.buttonCreateIncomeRecord);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -44,5 +70,8 @@
         }
 
         #endregion
+
+        private Button buttonCreateIncomeRecord;
+        private Button buttonCreateExpenseRecord;
     }
 }
--- BudgetApp.UI/Windows/MainWindow.cs
+++ BudgetApp.UI/Windows/MainWindow.cs
@@ -1,10 +1,22 @@
-﻿namespace BudgetApp.UI.Windows
+﻿using System.Diagnostics;
+
+namespace BudgetApp.UI.Windows
 {
     public partial class MainWindow : Form
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonCreateIncomeRecord_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Creating income record...");
+        }
+
+        private void ButtonCreateExpenseRecord_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Creating expense record...");
         }
     }
 }
