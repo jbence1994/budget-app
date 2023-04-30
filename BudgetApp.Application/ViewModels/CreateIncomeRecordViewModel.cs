1afddf9f74adfccbc3be37e0e9d90258265f5b42
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2023-04-30 12:31:42 +0200

    Override ToString() method for VMs

--- BudgetApp.Application/ViewModels/CreateIncomeRecordViewModel.cs
+++ BudgetApp.Application/ViewModels/CreateIncomeRecordViewModel.cs
@@ -4,5 +4,10 @@
     {
         public string Source { get; set; }
         public int Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Amount} {Source} {Comment} {Category}";
+        }
     }
 }
